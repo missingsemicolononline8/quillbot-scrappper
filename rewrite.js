99f16821ae46f47a968064606b4caddcb9059359
missingsemicolononline8 <76899808+missingsemicolononline8@users.noreply.github.com>
2023-07-24 19:52:19 +0530

    Debugging rewrite.js - readded sample puppeteer code

--- rewrite.js
+++ rewrite.js
@@ -16,31 +16,32 @@ const rewrite = async (res,textToSpin,headless) => {
     executablePath: process.env.NODE_ENV === 'production' ? process.env.PUPPETEER_EXECUTABLE_PATH : puppeteer.executablePath()});
   
   try {
-  const page = await browser.newPage();
+    const page = await browser.newPage();
 
-  // Navigate the page to a URL
-  await page.goto('https://developer.chrome.com/');
-
-  // Set screen size
-  await page.setViewport({width: 1080, height: 1024});
-
-  // Type into search box
-  await page.type('.search-box__input', 'automate beyond recorder');
-
-  // Wait and click on first result
-  const searchResultSelector = '.search-box__link';
-  await page.waitForSelector(searchResultSelector);
-  await page.click(searchResultSelector);
-
-  // Locate the full title with a unique string
-  const textSelector = await page.waitForSelector(
-    'text/Customize and automate'
-  );
-  const fullTitle = await textSelector?.evaluate(el => el.textContent);
-
-  console.log('The title of this blog post is "%s".', fullTitle);
-
-//await browser.close()
+    // Navigate the page to a URL
+    await page.goto('https://developer.chrome.com/');
+  
+    // Set screen size
+    await page.setViewport({width: 1080, height: 1024});
+  
+    // Type into search box
+    await page.type('.search-box__input', 'automate beyond recorder');
+  
+    // Wait and click on first result
+    const searchResultSelector = '.search-box__link';
+    await page.waitForSelector(searchResultSelector);
+    await page.click(searchResultSelector);
+  
+    // Locate the full title with a unique string
+    const textSelector = await page.waitForSelector(
+      'text/Customize and automate'
+    );
+    const fullTitle = await textSelector?.evaluate(el => el.textContent);
+  
+    // Print the full title
+    console.log('The title of this blog post is "%s".', fullTitle);
+  
+    await browser.close();
   res.send(fullTitle);
   }
   catch (e) {
